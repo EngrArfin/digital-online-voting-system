8a14fed622907d65cb10cf9f78ab2fabddd251da
EngrArfin <arfin.cse.green.edu.bd@gmail.com>
2023-06-01 21:40:09 +0600

    add home page footer

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -12,7 +12,6 @@ const firebaseConfig = {
   messagingSenderId: "999541665713",
   appId: "1:999541665713:web:0d6a7ee2f9def91fe89790"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export default app;
